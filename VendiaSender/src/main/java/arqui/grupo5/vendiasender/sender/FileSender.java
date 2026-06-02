2c2e489a2154df963cc1718ac62d8ff36423c983
Gerardo <gerarleonce-0703@outlook.es>
2026-06-02 18:08:42 -0500

    Cambios realizados para generar el ejecutable
    Básicamente se modificó los archivos del pom y 2 clases main

--- VendiaSender/src/main/java/arqui/grupo5/vendiasender/sender/FileSender.java
+++ VendiaSender/src/main/java/arqui/grupo5/vendiasender/sender/FileSender.java
@@ -18,7 +18,6 @@ import java.util.function.Consumer;
  * Envia las ventas pendientes copiando un archivo binario a la carpeta
  * compartida DATOS\ y espera la aparicion del archivo .ack correspondiente
  * como confirmacion de que VendiaUpdater las inserto en MySQL.
- *
  * Reemplaza al antiguo TcpSender que usaba sockets TCP.
  */
 public class FileSender {
